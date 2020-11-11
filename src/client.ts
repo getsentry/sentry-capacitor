a9aa524a4544fab7a3f49ed038972b1b5a95ae00
Cortney Thomas <thomas.cortney@gmail.com>
2020-11-11 12:08:00 -0500

    chore: finish up with js functionality for initial setup

--- src/client.ts
+++ src/client.ts
@@ -1,10 +1,7 @@
-import { BaseClient, Scope } from '@sentry/core';
-import { Event, EventHint } from '@sentry/types';
-import { SyncPromise } from '@sentry/utils';
+import { BaseClient } from '@sentry/core';
 
 import { CapacitorBackend } from './backend';
 import { CapacitorOptions } from './options';
-import { SDK_NAME, SDK_VERSION } from './version';
 
 /**
  * The Sentry Capacitor SDK Client.
@@ -19,30 +16,5 @@ export class CapacitorClient extends BaseClient<
    */
   public constructor(options: CapacitorOptions) {
     super(CapacitorBackend, options);
-  }
-
-  /**
-   * @inheritDoc
-   */
-  protected _prepareEvent(
-    event: Event,
-    scope?: Scope,
-    hint?: EventHint,
-  ): SyncPromise<Event | null> {
-    event.platform = event.platform || 'javascript';
-    event.sdk = {
-      ...event.sdk,
-      name: SDK_NAME,
-      packages: [
-        ...((event.sdk && event.sdk.packages) || []),
-        {
-          name: 'npm:sentry-capacitor',
-          version: SDK_VERSION,
-        },
-      ],
-      version: SDK_VERSION,
-    };
-
-    return super._prepareEvent(event, scope, hint);
   }
 }
--- src/wrapper.ts
+++ src/wrapper.ts
@@ -1,5 +1,6 @@
 import { Plugins } from '@capacitor/core';
 import { logger, SentryError } from '@sentry/utils';
+import { Breadcrumb, Severity, User } from '@sentry/types';
 
 import { CapacitorOptions } from './options';
 
@@ -32,6 +33,116 @@ export const NATIVE = {
     }
 
     return SentryCapacitor.startWithOptions(options);
+  },
+
+  /**
+   * Sets the user in the native scope.
+   * Passing null clears the user.
+   * @param key string
+   * @param value string
+   */
+  setUser(user: User | null): void {
+    if (!this.enableNative) {
+      return;
+    }
+    if (!this.isNativeClientAvailable()) {
+      throw this._NativeClientError;
+    }
+
+    // separate and serialze all non-default user keys.
+    let defaultUserKeys = null;
+    let otherUserKeys = null;
+    if (user) {
+      const { id, ip_address, email, username, ...otherKeys } = user;
+      defaultUserKeys = this._serializeObject({
+        email,
+        id,
+        ip_address,
+        username,
+      });
+      otherUserKeys = this._serializeObject(otherKeys);
+    }
+
+    SentryCapacitor.setUser(defaultUserKeys, otherUserKeys);
+  },
+
+  /**
+   * Sets a tag in the native module.
+   * @param key string
+   * @param value string
+   */
+  setTag(key: string, value: string): void {
+    if (!this.enableNative) {
+      return;
+    }
+    if (!this.isNativeClientAvailable()) {
+      throw this._NativeClientError;
+    }
+
+    const stringifiedValue =
+      typeof value === 'string' ? value : JSON.stringify(value);
+
+    SentryCapacitor.setTag(key, stringifiedValue);
+  },
+
+  /**
+   * Sets an extra in the native scope, will stringify
+   * extra value if it isn't already a string.
+   * @param key string
+   * @param extra any
+   */
+  setExtra(key: string, extra: unknown): void {
+    if (!this.enableNative) {
+      return;
+    }
+    if (!this.isNativeClientAvailable()) {
+      throw this._NativeClientError;
+    }
+
+    // we stringify the extra as native only takes in strings.
+    const stringifiedExtra =
+      typeof extra === 'string' ? extra : JSON.stringify(extra);
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    SentryCapacitor.setExtra(key, stringifiedExtra);
+  },
+
+  /**
+   * Adds breadcrumb to the native scope.
+   * @param breadcrumb Breadcrumb
+   */
+  addBreadcrumb(breadcrumb: Breadcrumb): void {
+    if (!this.enableNative) {
+      return;
+    }
+    if (!this.isNativeClientAvailable()) {
+      throw this._NativeClientError;
+    }
+
+    SentryCapacitor.addBreadcrumb({
+      ...breadcrumb,
+      // Process and convert deprecated levels
+      level: breadcrumb.level
+        ? this._processLevel(breadcrumb.level)
+        : undefined,
+      data: breadcrumb.data
+        ? this._serializeObject(breadcrumb.data)
+        : undefined,
+    });
+  },
+
+  /**
+   * Clears breadcrumbs on the native scope.
+   */
+  clearBreadcrumbs(): void {
+    if (!this.enableNative) {
+      return;
+    }
+    if (!this.isNativeClientAvailable()) {
+      throw this._NativeClientError;
+    }
+
+    SentryCapacitor.clearBreadcrumbs();
   },
 
   /**
@@ -107,6 +218,32 @@ export const NATIVE = {
     return serialized;
   },
 
+  /**
+   * Convert js severity level which has critical and log to more widely supported levels.
+   * @param level
+   * @returns More widely supported Severity level strings
+   */
+  _processLevel(level: Severity): Severity {
+    if (level === Severity.Critical) {
+      return Severity.Fatal;
+    }
+    if (level === Severity.Log) {
+      return Severity.Debug;
+    }
+
+    return level;
+  },
+
+  /**
+   * Checks whether the SentryCapacitor module is loaded.
+   */
+  isModuleLoaded(): boolean {
+    return !!SentryCapacitor;
+  },
+
   _DisabledNativeError: new SentryError('Native is disabled'),
+  _NativeClientError: new SentryError(
+    "Native Client is not available, can't start on native.",
+  ),
   enableNative: true,
 };
