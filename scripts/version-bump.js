a99b25d81c5cd676f7c43c3d4baa595fda2911e0
jennmueng <ldrwarlock@gmail.com>
2021-06-08 17:06:30 +0700

    meta: Craft yml and pre release version bump script

---
+++ scripts/version-bump.js
@@ -0,0 +1,15 @@
+const replace = require("replace-in-file");
+
+const pjson = require("../package.json");
+
+replace({
+  files: ["src/js/version.ts"],
+  from: /\d+\.\d+.\d+(?:-\w+(?:\.\w+)?)?/g,
+  to: pjson.version,
+})
+  .then((changedFiles) => {
+    console.log("Modified files:", changedFiles.join(", "));
+  })
+  .catch((error) => {
+    console.error("Error occurred:", error);
+  });
