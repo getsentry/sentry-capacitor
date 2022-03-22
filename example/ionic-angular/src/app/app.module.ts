91d5aedc06aa0c6fd691326f63529822702c3da0
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2022-03-22 16:30:03 -0300

    udate sample 1 to angular 13

--- example/ionic-angular/src/app/app.module.ts
+++ example/ionic-angular/src/app/app.module.ts
@@ -17,7 +17,7 @@ import { AppComponent } from './app.component';
 Sentry.init(
   {
     dsn:
-      'https://4079af8b316240ea9453eb0a23b715cc@o447951.ingest.sentry.io/5522756',
+      'https://4079af8b316240ea9453eb0a23b715cc@o447951.ingest.sentry.io/55A22756',
     // An array of strings or regexps that'll be used to ignore specific errors based on their type/message
     ignoreErrors: [/MiddleEarth_\d\d/, 'RangeError'],
     // To see what the Sentry SDK is doing; Helps when setting things up
@@ -40,19 +40,18 @@ Sentry.init(
 );
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    /* Provide the @sentry/angular error handler */
-    {
-      provide: ErrorHandler,
-      useValue: createErrorHandler(),
-    },
-  ],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+        /* Provide the @sentry/angular error handler */
+        {
+            provide: ErrorHandler,
+            useValue: createErrorHandler(),
+        },
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {}
