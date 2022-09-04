0f776c2eb202cd987fa2dacdd0bc138046c96a6e
Lucas Zimerman Fraulob <lucas-zimerman1@hotmail.com>
2022-09-04 16:18:33 +0900

    nit

--- scripts/check-uncommitted-changes.ps1
+++ scripts/check-uncommitted-changes.ps1
@@ -1,7 +1,7 @@
 #Resumed list of what changed
 $output = git status --porcelain
 
-if ($null -ne $output) {
+if ('' -ne $output) {
   Write-Warning "There are uncommited changes to your code, please revise the changes before commiting"
   $diffArray = git diff
   $diff = [system.String]::Join("`n", $diffArray)
