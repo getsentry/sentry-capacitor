36dfcd8ff9f9c6c453a965a311b260bdb24ca5dd
Bruno Garcia <bruno@brunogarcia.com>
2023-01-24 11:06:59 -0500

    replay: be explicit on sample rates

--- example/ionic-angular/src/app/app.module.ts
+++ example/ionic-angular/src/app/app.module.ts
@@ -40,6 +40,12 @@ Sentry.init(
     // We recommend adjusting this value in production, or using tracesSampler
     // for finer control
     tracesSampleRate: 1.0,
+    // This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample at a lower rate in production
+    replaysSessionSampleRate: 0.1,
+    // If the entire session is not sampled, use the below sample rate to sample
+    // sessions when an error occurs.
+    replaysOnErrorSampleRate: 1.0,
   },
   sentryAngularInit,
 );
