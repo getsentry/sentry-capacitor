d56730307084a861642cfd7c6a009aa2b8186204
Lucas <lucas-zimerman1@hotmail.com>
2023-12-18 15:32:11 -0300

    remove old script

--- scripts/update-javascript.sh
+++ scripts/update-javascript.sh
@@ -8,4 +8,4 @@ packages=('@sentry/browser' '@sentry/core' '@sentry/integrations' '@sentry/types
 #https://github.com/getsentry/sentry-capacitor/issues/511
 packages+=('@sentry/hub' '@sentry/tracing')
 packages+=('@sentry-internal/eslint-config-sdk' '@sentry-internal/eslint-plugin-sdk' '@sentry-internal/typescript')
-. $(dirname "$0")/update-package-json.sh && . $(dirname "$0")/update-peer-package-json.sh
+. $(dirname "$0")/update-package-json.sh
