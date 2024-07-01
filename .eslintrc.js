743c4d24ac3d8ca7db9311c1988aa9b3d6e392e0
Lucas <lucas-zimerman1@hotmail.com>
2024-07-01 14:35:24 -0300

    fix test build rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,7 @@ module.exports = {
         '@typescript-eslint/no-empty-function': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/ban-ts-comment': 'off',
+        '@typescript-eslint/unbound-method': 'off',
       },
     },
     {
