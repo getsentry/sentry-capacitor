4873d8298767bac4dc1b551660e04e266a220311
Lucas <lucas-zimerman1@hotmail.com>
2024-09-09 15:38:06 -0300

    remove old dependencies

--- scripts/update-javascript-siblings.sh
+++ scripts/update-javascript-siblings.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 tagPrefix=''
 updatePeerPackages=1
 repo="https://github.com/getsentry/sentry-javascript.git"
-packages=('@sentry/react' '@sentry/vue' '@sentry/angular' '@sentry/angular-ivy')
+packages=('@sentry/react' '@sentry/vue' '@sentry/angular')
 . $(dirname "$0")/update-package-json.sh
