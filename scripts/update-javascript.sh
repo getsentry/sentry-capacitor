0dff0d3ed6a9f98b87f05f90c93150949e7bf8c6
LucasZF <lucas-zimerman1@hotmail.com>
2025-02-11 14:33:58 -0300

    ref: remove sentry utils update

--- scripts/update-javascript.sh
+++ scripts/update-javascript.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 tagPrefix=''
 repo="https://github.com/getsentry/sentry-javascript.git"
-packages=('@sentry/browser' '@sentry/core' '@sentry/types' '@sentry/utils')
+packages=('@sentry/browser' '@sentry/core' '@sentry/types')
 packages+=('@sentry-internal/eslint-config-sdk' '@sentry-internal/eslint-plugin-sdk' '@sentry-internal/typescript')
 . $(dirname "$0")/update-package-json.sh
