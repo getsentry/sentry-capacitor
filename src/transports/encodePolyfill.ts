c56677ffd858222d29c7186b0b5abbfac617c546
lucas <lucas-zimerman1@hotmail.com>
2025-03-17 16:59:42 -0300

    use the default sdk version from @sentry/core

--- src/transports/encodePolyfill.ts
+++ src/transports/encodePolyfill.ts
@@ -1,3 +1,5 @@
+import { SDK_VERSION } from '@sentry/core';
+
 import { CAP_GLOBAL_OBJ } from '../utils/webViewUrl';
 import { utf8ToBytes } from '../vendor';
 
@@ -9,8 +11,7 @@ export const useEncodePolyfill = (): void => {
       (typeof CAP_GLOBAL_OBJ)['__SENTRY__']
     >) = {};
   }
-  const capacitorSiblingCarrier = globalCarriers?.version && globalCarriers['9.0.0'];
-
+  const capacitorSiblingCarrier = CAP_GLOBAL_OBJ.__SENTRY__?.[SDK_VERSION];
 
   if (capacitorSiblingCarrier) {
     capacitorSiblingCarrier.encodePolyfill = encodePolyfill;
