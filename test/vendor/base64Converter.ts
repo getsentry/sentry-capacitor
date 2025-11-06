7a875d541b0b2e91ac11017f5c9966ac92c88799
lucas <lucas-zimerman1@hotmail.com>
2025-11-06 16:28:48 +0000

    missing file

---
+++ test/vendor/base64Converter.ts
@@ -0,0 +1,9 @@
+export function Base64StringToString(base64String: string): string {
+  const binaryString = atob(base64String);
+  const bytes = new Uint8Array(binaryString.length);
+  for (let i = 0; i < binaryString.length; i++) {
+    bytes[i] = binaryString.charCodeAt(i);
+  }
+  return new TextDecoder().decode(bytes);
+}
+
