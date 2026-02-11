50a6e2238acdf9a4c1e85e1693ea67145d343369
lucas <lucas-zimerman1@hotmail.com>
2026-02-11 14:30:40 +0000

    bump dependenceis from sentry vue 3 app

--- example/ionic-vue3/ios/App/CapApp-SPM/Package.swift
+++ example/ionic-vue3/ios/App/CapApp-SPM/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "6.2.1"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "8.0.2"),
         .package(name: "CapacitorApp", path: "../../../node_modules/@capacitor/app"),
         .package(name: "CapacitorHaptics", path: "../../../node_modules/@capacitor/haptics"),
         .package(name: "CapacitorKeyboard", path: "../../../node_modules/@capacitor/keyboard"),
